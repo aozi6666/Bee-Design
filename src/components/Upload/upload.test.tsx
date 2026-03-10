9761e646b53a2b6d4bb9428393dd72513df1e596
zhangao <2212433186@qq.com>
2026-03-10 11:47:17 +0800

    3.10

--- src/components/Upload/upload.test.tsx
+++ src/components/Upload/upload.test.tsx
@@ -1,5 +1,4 @@
 import '@testing-library/jest-dom/extend-expect'
-import React from 'react'
 import axios from 'axios'
 import { render, RenderResult, fireEvent, waitFor } from '@testing-library/react'
 

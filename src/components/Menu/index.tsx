bcfc7b995fcd0b85ffb259ed4a88d7f54f23d825
zhangao <2212433186@qq.com>
2026-03-14 14:00:43 +0800

    3.14

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,7 +1,8 @@
-import { FC } from 'react'
-import Menu, { MenuProps } from './menu'
-import SubMenu, { SubMenuProps } from './subMenu'
-import MenuItem, { MenuItemProps } from './menuItem'
+// React 组件组合模式（Compound Component Pattern）
+import type { FC } from 'react'
+import Menu, { type MenuProps } from './menu'
+import SubMenu, { type SubMenuProps } from './subMenu'
+import MenuItem, { type MenuItemProps } from './menuItem'
 
 export type IMenuComponent = FC<MenuProps> & {
   Item: FC<MenuItemProps>,
